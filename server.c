5306e2090bd5d07f2fca73e826d4e944b2eaa291
MathieuGalant <35834776+MathieuGalant@users.noreply.github.com>
2018-09-06 09:51:48 +0200

    Add files via upload

--- server.c
+++ server.c
@@ -13,7 +13,7 @@ void getTokenServer(char *tokenFile)
 {
     char tokenServer[100];
     printf("ok");
-    sprintf(tokenServer,"curl -o %s -X GET \"localhost:8000/api/auth/?username=pi&password=raspberry&imei=1234\"",tokenFile);
+    sprintf(tokenServer,"curl -o %s -X GET \"10.237.104.63:8000/api/auth/?username=pi&password=raspberry&imei=1234\"",tokenFile);
     printf("%s\n",tokenServer);
     pid_t pid=fork();
     if (pid==0)
@@ -27,7 +27,7 @@ void getAlarmServer(int ID, char *alarmFile, char *token)
 {
     char alarmServer[200];
     printf("ok");
-    sprintf(alarmServer,"curl -o %s -X GET \"http://localhost:8000/api/alarm/%d/?token=%s\"",alarmFile,ID,token);
+    sprintf(alarmServer,"curl -o %s -X GET \"10.237.104.63:8000/api/alarm/%d/?token=%s\"",alarmFile,ID,token);
     printf("%s\n",alarmServer);
     system(alarmServer);
 }
@@ -37,7 +37,7 @@ void getMusicFilesServer(int ID, char *musicFile, char *token) // need update
 {
     char musicServer[200];
     printf("ok");
-    sprintf(musicServer,"curl -o %s -X GET \"http://localhost:8000/api/files/song/%d/?token=%s\"",musicFile,ID,token);
+    sprintf(musicServer,"curl -o %s -X GET \"10.237.104.63:8000/api/files/song/%d/?token=%s\"",musicFile,ID,token);
     printf("%s\n",musicServer);
     system(musicServer);
 }
@@ -46,7 +46,7 @@ void getVideoFilesServer(int ID, char *videoFile, char *token) // need update
 {
     char videoServer[200];
     printf("ok");
-    sprintf(videoServer,"curl -o %s -X GET \"http://localhost:8000/api/files/video/%d/?token=%s\"",videoFile,ID,token);
+    sprintf(videoServer,"curl -o %s -X GET \"10.237.104.63:8000/api/files/video/%d/?token=%s\"",videoFile,ID,token);
     printf("%s\n",videoServer);
     system(videoServer);
 }
